80acf01c959c7810a61cfea754c3df0b47ca4440
tiny <136625214@qq.com>
2018-10-27 15:57:04 +0800

    Site updated: 2018-10-27 15:57:02

--- js/plugin.js
+++ js/plugin.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-  console.log("stay hungry , stay foolish . ")
+$(document).ready(function () {
+    console.log("stay hungry , stay foolish . ");
+    _layoutQRCode();
 });
+
+
+function _layoutQRCode() {
+    var path = window.location.href;
+    if (path.endsWith("/about/")) {
+        var img = $("p img");
+        img.css("width", 90);
+        img.css("height", 90);
+        img.css("margin", 0);
+    }
+}
